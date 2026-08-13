88970aa4ba9da560b5778e28ae769e3ec9370d27
AIWintermuteAI <dmitrywat@gmail.com>
2026-08-13 12:40:11 +0000

    fix warning and modify params

--- video_to_ros2/video_publisher_node.py
+++ video_to_ros2/video_publisher_node.py
@@ -11,23 +11,26 @@ class VideoPublisher(Node):
     def __init__(self):
         super().__init__(node_name="video_publisher")
         # TODO - change parameters
-        self.declare_parameter("otopic_video", "/video")
-        self.otopic_video = self.get_parameter("otopic_video").value
+        self.declare_parameter("publish_topic", "/camera/image_raw")
+        self.publish_topic = self.get_parameter("publish_topic").value
+        self.declare_parameter("loop", True)
+        self.loop = self.get_parameter("loop").value
+        self.get_logger().info(f"Publishing video to topic {self.publish_topic}, loop={self.loop}")
 
         self.cv_bridge = CvBridge()
 
         self.frame_publisher = self.create_publisher(
             Image,
-            self.otopic_video,
+            self.publish_topic,
             0
         )
 
-        self.declare_parameter("ifile_video", "videos/video.mp4")
-        self.ifile_video = self.get_parameter("ifile_video").value
+        self.declare_parameter("video_file", "videos/video.mp4")
+        self.video_file = self.get_parameter("video_file").value
 
         picture_format_tuples = (".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".gif")
 
-        if self.ifile_video.endswith(picture_format_tuples):
+        if self.video_file.endswith(picture_format_tuples):
             self.load_image()
         else:
             self.load_video()
@@ -39,7 +42,7 @@ class VideoPublisher(Node):
 
 
     def load_image(self):
-        img = cv2.imread(self.ifile_video)
+        img = cv2.imread(self.video_file)
 
         sleep_time = (1.0/60.0)
 
@@ -49,7 +52,7 @@ class VideoPublisher(Node):
 
 
     def load_video(self):
-        capture = cv2.VideoCapture(self.ifile_video)
+        capture = cv2.VideoCapture(self.video_file)
 
         if not capture.isOpened():
             self.get_logger().error("Failed to open video file...")
@@ -75,12 +78,16 @@ class VideoPublisher(Node):
                 # self.get_logger().info(f"Loading took less time than expected. Waiting {wait_time} ms")
                 time.sleep(1.0/wait_time)
 
-
             if not ret:
-                break
+                if self.loop:
+                    self.get_logger().info("Video finished, rewinding...")
+                    capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                    continue
+                else:
+                    break
+                    self.get_logger().info("Video finished.")
 
         capture.release()
-        self.get_logger().info("Video finished.")
 
 
 def main(args=None):
